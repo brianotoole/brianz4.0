255c98a04d540d70c92b2b1515abe780c69d54f4
Brian O'Toole <brian@sparxoo.com>
2017-12-06 10:55:14 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+
+    <nav class="nav">
+      <div class="nav-left">
+        <router-link to="/" class="nav-item logo">LOGO</router-link>
+      </div><!--/.nav-left-->
+      <div class="nav-right">
+        <router-link to="/" class="nav-item is-tab is-hidden-mobile">Home</router-link>
+        <router-link to="/users/:1" class="nav-item is-tab is-hidden-mobile">Users 1</router-link>
+        <router-link to="/users/:2" class="nav-item is-tab is-hidden-mobile">Users 2</router-link>
+      </div><!--/.nav-right-->
+    </nav>
+
+  <router-view></router-view>
+
+</div><!--/.app-->
+</template>
+
+<script>
+export default {
+  //name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import './assets/scss/app.scss';
+.header {
+  width: 100%;
+  margin: 0;
+  padding: calc-em(20px) 0;
+  background: $black;
+  border-bottom: 1px solid $primary;
+}
+.nav {
+  display: flex;
+  justify-content: space-between;
+  padding: calc-em(20px) calc-em(10px);
+  background-color: $black;
+  .nav-left,
+  .nav-right {
+    width: 100%;
+  }
+  @include breakpoint(md) {
+    .nav-left {
+      width: 33.333%;
+    }
+    .nav-right {
+      width: auto;
+      margin-left: auto;
+    }
+  }
+  a {
+    color: $white;
+    text-decoration: none;
+    padding: 0 calc-em(20px);
+  }
+}
+</style>
---
+++ src/Users.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Users</h1>
+        <h2 class="subtitle"><strong>User Id:</strong> {{ $route.params.userId }}</h2>
+        <button @click="goHome">Back to Homepage</button>
+      </div>
+    </div>
+  </section>
+</div>
+</template>
+
+<script>
+  export default {
+    watch: {
+      '$route' (to, from) {
+        //console.log(this.$route.params.userId);
+      }
+    },
+    created() {
+      //console.log(this.$route.params.userId);
+    },
+    methods: {
+      goHome() {
+        this.$router.push('/');
+      }
+    }
+  }
+</script>
