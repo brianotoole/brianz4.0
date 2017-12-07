21bf6ba3536f58d70946743c7e62ff4026d26875
Brian O'Toole <brian@sparxoo.com>
2017-12-07 14:47:40 -0500

    added simple flex-grid

--- src/Home.vue
+++ src/Home.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
   <div class="wrapper container-fluid">
-    <div class="row">
-      <div class="col-xs-12">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-      </div>
+    <div class="flex-wrap">
+      <div class="column">One</div>
+      <div class="column">Two</div>
+      <div class="column">Three</div>
     </div>
   </div>
+
 </div>
 </template>
