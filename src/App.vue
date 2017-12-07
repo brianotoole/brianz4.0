c0ac436bf9b76504ac4a7784ce9602fe013312dc
Brian O'Toole <brian@sparxoo.com>
2017-12-07 17:00:23 -0500

    completed nav toggle

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,30 @@
     <div class="col-xs-6 logo">
       <router-link to="/" class="nav-item logo">{{site}}</router-link>
     </div><!--/.col-->
-    <div class="col-xs-6 nav-primary">
-     <li v-for="nav in navs">
-      <router-link :to="`/${nav.link}`" class="nav-item">{{nav.title}}</router-link>
-     </li>
-     <div class="nav-toggle row end-xs middle-xs" @click="toggleNav = !toggleNav" v-bind:class="{ active: toggleNav }">
+    <div class="col-xs-6 end-xs">
+      <ul class="nav-primary">
+        <li v-for="nav in navs">
+          <router-link :to="`/${nav.link}`" class="nav-item">{{nav.title}}</router-link>
+        </li>
+      </ul><!--/.nav-primary-->
+      <div class="nav-toggle" @click="toggleNav = !toggleNav" v-bind:class="{ active: toggleNav }">
         <span class="line line-1"></span>
         <span class="line line-2"></span>
         <span class="line line-3"></span>
      </div><!--/.nav-toggle-->
-    </div><!--/.col-->
+     </div><!--/.col-->
   </div><!--/.row-->
 </header>
+
+<div class="nav-mobile row" v-bind:class="{ active: toggleNav }">
+  <div class="col-xs-12">
+    <ul class="nav-mobile-wrap">
+        <li v-for="nav in navs">
+          <router-link :to="`/${nav.link}`" class="nav-item">{{nav.title}}</router-link>
+        </li>
+      </ul><!--/.nav-primary-->
+  </div><!--/.col-->
+</div><!--/.nav-mobile-->
 
   <router-view></router-view>
 
