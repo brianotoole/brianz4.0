c5e0bf3b41c97e2286bccf9a42f1124fb5eaae1a
Brian O'Toole <brian@sparxoo.com>
2017-12-07 21:53:50 -0500

    testing brand styles / colors

--- src/Home.vue
+++ src/Home.vue
@@ -3,12 +3,11 @@
 
 <div class="page-home">
 
-<section class="section-wrap section-header vertical-center bg-img-cover has-overlay" style="background-image:url('./src/assets/img/home-bg.jpg');">
+<section class="section-wrap section-header full-height vertical-center" style="background-image:url('https://images.unsplash.com/photo-1504541891213-1b1dfdadb739?auto=format&fit=crop&w=1650&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D');">
   <div class="container">
-    <div class="row text-white">
-      <div class="col-xs-12 col-sm-8">
-        <h1 class="section-title">Making things better. Together.</h1>
-        <p class="section-subtitle">I help brands build recognition using creative strategy and modern design.</p>
+    <div class="row text-center">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+        <h1 class="section-title">Helping brands connect with audiences through intelligent design &amp; technology</h1>
       </div><!--/.col-->
     </div><!--<!--/.row-->
   </div><!--/.container-->
@@ -17,11 +16,12 @@
 <section class="section-wrap">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-sm-4">1</div><!--/.col-->
-      <div class="col-xs-12 col-sm-4">2</div><!--/.col-->
-      <div class="col-xs-12 col-sm-4">3</div><!--/.col-->
+      <div class="col-xs-12 col-sm-7">1</div><!--/.col-->
+      <div class="col-xs-12 col-sm-5">
+        <h3 class="section-title">Creative technology parters for your brand</h3>
+      </div><!--/.col-->
     </div><!--/.row-->
-  </div><!--/.container-->
+  </div><!--/./container-->
 </section>
 
 
