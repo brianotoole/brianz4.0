b32340ee37befa300fa0c5dd0af2908829c1b958
Brian O'Toole <brian@sparxoo.com>
2017-12-07 19:19:14 -0500

    added fonts + updated container

--- src/Home.vue
+++ src/Home.vue
@@ -5,10 +5,10 @@
 
 <section class="section-wrap section-header full-height vertical-center bg-primary has-overlay">
   <div class="container">
-    <div class="row">
-      <div class="col-xs-12 text-center">
-        <h1 class="section-title">Hero Title</h1>
-        <p class="section-subtitle">subtitle</p>
+    <div class="row text-center">
+      <div class="col-xs-12 col-sm-offset-2 col-sm-8 text-center">
+        <h1 class="section-title">Inspired design. Bold solutions. Bottom-line results.</h1>
+        <p class="section-subtitle">I help brands build recognition using creative strategy and modern design.</p>
       </div><!--/.col-->
     </div><!--<!--/.row-->
   </div><!--/.container-->
